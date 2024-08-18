26daabf88aa796e9ef5d073e36e4f347e5ae979c
Thomaz Henrique <treimberg7@gmail.com>
2024-08-17 23:37:46 -0300

    Implementing Query

---
+++ CatalogGraphQL/Queries/Query.cs
@@ -0,0 +1,15 @@
+﻿using CatalogGraphQL.Core.Entities;
+using CatalogGraphQL.Core.Repositories.Interfaces;
+using HotChocolate;
+
+namespace CatalogGraphQL.Queries
+{
+    public class Query
+    {
+        public Task<IEnumerable<Product>> GetProductsAsync([Service] IProductRepository productRepository) =>
+            productRepository.GetAllAsync();
+
+        public Task<Product> GetProductById(string id, [Service] IProductRepository productRepository) =>
+            productRepository.GetByIdAsync(id);
+    }
+}
