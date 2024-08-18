3f1687905924bda61585d8ea134936ac40029239
Thomaz Henrique <treimberg7@gmail.com>
2024-08-17 23:30:25 -0300

    Implementing Core and Infra

---
+++ CatalogGraphQL.Infrastructure/Data/Interfaces/ICatalogContext.cs
@@ -0,0 +1,11 @@
+﻿using CatalogGraphQL.Core.Entities;
+using MongoDB.Driver;
+
+namespace CatalogGraphQL.Infrastructure.Data.Interfaces
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Category> Categories { get; }
+        IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ CatalogGraphQL.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using CatalogGraphQL.Core.Entities;
+using CatalogGraphQL.Core.Repositories.Interfaces;
+using CatalogGraphQL.Infrastructure.Data.Interfaces;
+using MongoDB.Driver;
+
+namespace CatalogGraphQL.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ICatalogContext catalogContext;
+
+        public CategoryRepository(ICatalogContext catalogContext)
+        {
+            this.catalogContext = catalogContext;
+        }
+
+        public async Task<Category> GetById(string id)
+        {
+            var filter = Builders<Category>.Filter.Eq(_ => _.Id, id);
+
+            return await this.catalogContext.Categories.Find(filter).FirstOrDefaultAsync();
+        }
+    }
+}
